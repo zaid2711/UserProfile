784d23538d4386a976c6a9d8816ab6f0760412ba
Zaid Ahmed <zaidahmedmohd27@gmail.com>
2022-11-23 21:06:54 -0500

    changes

--- inc/header.php
+++ inc/header.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" >
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" ></script>
     <!-- add our custom CSS -->
-    <link rel="stylesheet" href="\csss\style.css">
+    <link rel="stylesheet" href="\css\style.css">
 </head>
 <body>
 <header>
